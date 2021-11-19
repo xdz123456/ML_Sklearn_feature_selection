d6a0bea3095da38106f6f385182876d0ba16007c
Dongzhan Xie <2663048x@student.gla.ac.uk>
2021-11-19 23:25:53 +0000

    Add variance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_selection import VarianceThreshold
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
 
@@ -17,14 +18,21 @@ labels_df = pd.read_csv("labels.txt", header=None)
 feature = np.array(feature_df)[0]
 data_df.columns = feature
 # Add the labels on the whole data
-labels = np.array(labels_df)
-data_df["labels"] = labels
+labels_df.columns = ["labels"]
+
+# Add labels to the data set
+# labels = np.array(labels_df)
+# data_df["labels"] = labels
 
 
 # The data set already finished the data normalization
 # One Hot for thr data
 # data = OneHotEncoder().fit_transform(data_df)
 
+# Feature Selection with variance
+data_with_variance = VarianceThreshold(threshold=3).fit_transform(data_df)
+
+
 # Wrapper
-RFE(estimator=LogisticRegression(), n_features_to_select=2).fit_transform(data_df)
+# data_with_wrapper = RFE(estimator=LogisticRegression(), n_features_to_select=2).fit_transform(data_df.values, labels_df.values)
 print("0000")
